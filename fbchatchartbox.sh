b58fcfe33063463f66dbcaa54e9f5448fadadfe5
Ian Dalton <iain.dalton@gmail.com>
2014-05-30 12:26:22 -0600

    Add box drawing version

---
+++ fbchatchartbox.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+NAME=$1
+SHIFT=${2-0}
+TIMESTAMPS=$(</dev/stdin grep -A1 "$NAME" | grep ":" | cut -c5-)
+DAYS="Mon Tue Wed Thu Fri Sat Sun"
+SHADES="░▒▓█"
+MAX=$(echo "$TIMESTAMPS" | fb-shift-ts.py "$SHIFT" | sort | uniq -c | sort -rn | head -1 \
+      | cut -c-7)
+
+for row in {0..1}; do
+    printf "    "
+    for hour in {00..23}; do
+        printf ${hour:$row:1}
+    done
+    echo
+done
+for day in $DAYS; do
+    printf "$day "
+    for hour in {00..23}; do
+        count=$(echo "$TIMESTAMPS" | fb-shift-ts.py "$SHIFT" | sort | uniq -c \
+                | grep "$day $hour" | cut -c1-7 | tr -d ' ')
+        if [ -z "$count" ]; then
+            printf " "
+        else
+            i=$(printf "%.0f" $(echo "scale=1; (${#SHADES}-1)*$count/$MAX + 0.5" | bc))
+            printf "${SHADES:$i:1}"
+        fi
+    done
+    echo
+done
